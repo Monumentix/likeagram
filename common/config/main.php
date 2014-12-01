fc8e345ab2b030ea969b8946ddf10fda2b962dad
Rizza <Rizza@elGuopo.monumentix.com>
2014-12-01 02:22:23 -0500

    Initial Commit of our Boilerplate Clone to our Likeagram Repo

---
+++ common/config/main.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+
+    'modules'=>[
+	'user' => [
+            'class' => 'dektrium\user\Module',
+            'enableUnconfirmedLogin' => false,
+            'confirmWithin' => 21600,
+            'cost' => 12,
+            'admins' => ['Admin']
+        ],
+   ],
+    
+];
