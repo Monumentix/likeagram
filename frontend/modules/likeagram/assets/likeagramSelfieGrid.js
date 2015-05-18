ad2ad73a7decc830c3f08017cbaffc927d83fedc
Brian Ridsdale <brian@monumentixwebdesign.com>
2015-05-18 14:12:59 -0400

    UPDATED: turned off hidden class in selfie page on new appends

--- frontend/modules/likeagram/assets/likeagramSelfieGrid.js
+++ frontend/modules/likeagram/assets/likeagramSelfieGrid.js
@@ -1,170 +1,169 @@
-/* 
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-    
+
     /*
     var iso = new Isotope('#selfieGrid',{
         itemSelector: '.selfieItem',
-        /*(layoutMode: 'fitRows'            
+        /*(layoutMode: 'fitRows'
           masonry: {
             columnWidth: 185
             }
     });
     iso.bindResize();
     */
-    
+
     var timeout = setInterval(selfieGrid,6500);
     var selfiePage = 1;
-    
+
     selfieGrid();
-    
-    function selfieGrid(){            
-        //Build our ISO OBJ                           
+
+    function selfieGrid(){
+
+
+
+        //Build our ISO OBJ
         $.get('selfie/next',{}, function(data){
-        //add data to placeholder div to turn to dom object                
-        //and assign the correct page numbers 
-        $('#nextPagePlaceholder').append(data);                                       
+        //add data to placeholder div to turn to dom object
+        //and assign the correct page numbers
+        $('#nextPagePlaceholder').append(data);
             if(selfiePage === 1){
-                $('#nextPagePlaceholder').children().addClass(' first hidden');                    
+                $('#nextPagePlaceholder').children().addClass(' first ');
             }
             if(selfiePage === 2){
-                $('#nextPagePlaceholder').children().addClass(' second hidden');
+                $('#nextPagePlaceholder').children().addClass(' second ');
             }
             if(selfiePage === 3){
-                $('#nextPagePlaceholder').children().addClass(' third hidden');
+                $('#nextPagePlaceholder').children().addClass(' third ');
             }
                 //Create our grid oject
-                var selfieGrid = $('#selfieGrid');  
-                    
+                var selfieGrid = $('#selfieGrid');
+
                 var nextPageGrid = $('#nextPagePlaceholder');
-                var newElements = nextPageGrid.children().slice(0);                
-                
+                var newElements = nextPageGrid.children().slice(0);
 
-
-
-                    
                 // selfieGrid.prepend(newElements);
-                
+
                 $.each(newElements, function(i, val){
                     selfieGrid.prepend(val);
-                    iso.prepended(val);  
+                    iso.prepended(val);
                     $('#selfieGrid .tagWall').addClass('reveal');
                     /*
                     selfieGrid.prepend(val);
-                        var imgLoad = imagesLoaded(selfieGrid);                        
-                        imgLoad.on('done', function(instance){       
-                        //selfieGrid.height(window.innerHeight);   
-                        iso.prepended(val);    
+                        var imgLoad = imagesLoaded(selfieGrid);
+                        imgLoad.on('done', function(instance){
+                        //selfieGrid.height(window.innerHeight);
+                        iso.prepended(val);
                             //Get our old items of the dom
                             if(selfiePage === 1){
-                                $('#selfieGrid .second').remove();                                            
+                                $('#selfieGrid .second').remove();
                                 selfiePage = 2;
                             }else if(selfiePage === 2){
-                                $('#selfieGrid .third').remove();                        
+                                $('#selfieGrid .third').remove();
                                 selfiePage = 3;
                             }else if(selfiePage === 3){
-                                $('#selfieGrid .first').remove();                        
+                                $('#selfieGrid .first').remove();
                                 selfiePage = 1;
                             }
-                        iso.layout();     
-                    }); 
+                        iso.layout();
+                    });
                     */
-                   
+
                 });
-                
-                var imgLoad = imagesLoaded(selfieGrid);  
-                imgLoad.on('done', function(instance){                            
-                    //Get our old items of the dom                            
+
+                var imgLoad = imagesLoaded(selfieGrid);
+                imgLoad.on('done', function(instance){
+                    //Get our old items of the dom
                     if(selfiePage === 1){
-                        $('#selfieGrid .second').remove();                                            
+                        $('#selfieGrid .second').remove();
                         selfiePage = 2;
                     }else if(selfiePage === 2){
-                        $('#selfieGrid .third').remove();                        
+                        $('#selfieGrid .third').remove();
                         selfiePage = 3;
                     }else if(selfiePage === 3){
-                        $('#selfieGrid .first').remove();                        
+                        $('#selfieGrid .first').remove();
                         selfiePage = 1;
-                    }                                                
+                    }
                     $('#selfieGrid').children().removeClass('hidden');
-                    iso.layout();       
+                    iso.layout();
                 });
-                
+
                 /*
-                var imgLoad = imagesLoaded(selfieGrid);                        
-                    imgLoad.on('done', function(instance){       
-                    //selfieGrid.height(window.innerHeight);   
-                    iso.prepended(newElements);    
+                var imgLoad = imagesLoaded(selfieGrid);
+                    imgLoad.on('done', function(instance){
+                    //selfieGrid.height(window.innerHeight);
+                    iso.prepended(newElements);
                         //Get our old items of the dom
                         if(selfiePage === 1){
-                            $('#selfieGrid .second').remove();                                            
+                            $('#selfieGrid .second').remove();
                             selfiePage = 2;
                         }else if(selfiePage === 2){
-                            $('#selfieGrid .third').remove();                        
+                            $('#selfieGrid .third').remove();
                             selfiePage = 3;
                         }else if(selfiePage === 3){
-                            $('#selfieGrid .first').remove();                        
+                            $('#selfieGrid .first').remove();
                             selfiePage = 1;
                         }
-                    iso.layout();     
-                }); 
+                    iso.layout();
+                });
                 */
-                 
-                 
-                 
-                    
-                //$('#nextPagePlaceholder .selfieItem').remove();                    
+
+
+
+
+                //$('#nextPagePlaceholder .selfieItem').remove();
                 //console.log(newElements);
-                //var oldElements = selfieGrid.children();                 
+                //var oldElements = selfieGrid.children();
                 //console.log(oldElements);
-                
-                
-                //$('#nextPagePlaceholder').remove().children();      
-                
-               // $('#selfieGrid').remove(oldElements);                
-                //document.getElementById('#selfieGrid').append('TEST');                                   
-            
-                //iso.prepended(newElements);                                     
-                
+
+
+                //$('#nextPagePlaceholder').remove().children();
+
+               // $('#selfieGrid').remove(oldElements);
+                //document.getElementById('#selfieGrid').append('TEST');
+
+                //iso.prepended(newElements);
+
                 /*
-                var imgLoad = imagesLoaded($('#selfieGrid').children());                        
-                    imgLoad.on('done', function(instance){                                        
-                    iso.layout();     
+                var imgLoad = imagesLoaded($('#selfieGrid').children());
+                    imgLoad.on('done', function(instance){
+                    iso.layout();
                 });   */
                 //$('#selfieGrid .selfieItem').addClass('old');
 
-                //isLoading = false;                             
+                //isLoading = false;
             }).fail(function(xhr, ajaxOptions, thrownError) { //any errors?
                 alert(thrownError); //alert with HTTP error
                 //isLoading = false;
-            }).done(function(){                                        
+            }).done(function(){
                     //alert('Hey there im done OK');
-            });                            
+            });
         }//end selfieGrid
-    
-    
 
-    function loadMediaModal($id){       
+
+
+    function loadMediaModal($id){
         var $mediaId = $id;
-        
+
         if($mediaId.length >0){
             //Lets try to load media data for this modal
-            var request = $.ajax({ 
-                url: '/likeagram/explore/media',        
+            var request = $.ajax({
+                url: '/likeagram/explore/media',
                 type: "POST",
                 cache: false,
-                data: {"ajax":true,"mediaid":$mediaId}, 
-                dataType: "html" 
-              });	
-              
+                data: {"ajax":true,"mediaid":$mediaId},
+                dataType: "html"
+              });
+
               $("body").toggleClass("wait");
-              request.done(function(request) { 
-                try{   
+              request.done(function(request) {
+                try{
                     //SANITIZE OR CHECK THIS RETURN BEFORE INSERTING
                     $('#mediaDetailModalWrapper').replaceWith(request);
-                    
+
                     //document.getElementById("mediaDetailModalWrapper").innerHTML = request;
-                    }//end try       
+                    }//end try
                 catch(ex){
                         if(varDEBUG === true){
                             console.log(ex);
@@ -176,9 +175,9 @@
                      $('#mediaDetailModal').modal('show');
                     }//end finally
               });//end request
-        }//end if $mediaId > 0 
-        
-        return false; 
-     
-       
-    }//end loadMediaModal   
+        }//end if $mediaId > 0
+
+        return false;
+
+
+    }//end loadMediaModal
